d9ff1605a743ba1684fec63f8beabff00b4db860
Torsten <apps+github@torsten-albrecht.de>
2022-01-31 00:38:39 +0100

    1.1

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,5 +1,6 @@
 package greetings
 
+/*
 import "fmt"
 
 // Hello returns a greeting for the named person.
@@ -7,4 +8,23 @@ func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
+}
+*/
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
