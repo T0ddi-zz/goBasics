bb6c9b67fc6eb9c1e98d50ea39dd405dbc0531d1
Torsten <apps+github@torsten-albrecht.de>
2022-01-31 00:41:47 +0100

    1.2

--- hello/hello.go
+++ hello/hello.go
@@ -14,6 +14,7 @@ func main() {
 }
 */
 
+/*
 import (
 	"fmt"
 	"log"
@@ -30,6 +31,34 @@ func main() {
 
 	// Request a greeting message.
 	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+}
+*/
+
+import (
+	"fmt"
+	"log"
+
+	"moohoo/greetings"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("T0ddi")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
